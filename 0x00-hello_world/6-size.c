7f73328f2db4f401e496db1d711ef11f405652ec
Hunt6666 <489@holbertonschool.com>
2018-06-20 21:42:40 +0000

    using multiple printf per line

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -7,10 +7,15 @@
  */
 int main(void)
 {
-printf("Size of a char: %d", sizeof(char), " byte(s)\n");
-printf("Size of an int: %d", sizeof(int), " byte(s)\n");
-printf("Size of a long int: %d", sizeof(long int), " byte(s)\n");
-printf("Size of a long long int: %d", sizeof(long long int), " bytes(s)\n");
-printf("Size of a float: %d", sizeof(float), " byte(s)\n");
+printf("Size of a char: %d", sizeof(char));
+printf( " byte(s)\n");
+printf("Size of an int: %d", sizeof(int));
+printf( " byte(s)\n");
+printf("Size of a long int: %d", sizeof(long int));
+printf( " byte(s)\n");
+printf("Size of a long long int: %d", sizeof(long long int));
+printf( " bytes(s)\n");
+printf("Size of a float: %d", sizeof(float));
+printf( " byte(s)\n");
 return (0);
 }
