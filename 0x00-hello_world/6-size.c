12b7d008b4694fc4cbbc6399c472b041ac9e4bac
Hunt6666 <489@holbertonschool.com>
2018-06-20 21:32:00 +0000

    forgot commas and %d for printf input

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -7,10 +7,10 @@
  */
 int main(void)
 {
-printf("Size of a char: " sizeof(char) " byte(s)\n");
-printf("Size of an int: " sizeof(int) " byte(s)\n");
-printf("Size of a long int: " sizeof(long int) " byte(s)\n");
-printf("Size of a long long int: " sizeof(long long int) " bytes(s)\n");
-printf("Size of a float: " sizeof(float) " byte(s)\n");
+printf("Size of a char: %d" , sizeof(char), " byte(s)\n");
+printf("Size of an int: %d" , sizeof(int), " byte(s)\n");
+printf("Size of a long int: %d" , sizeof(long int) , " byte(s)\n");
+printf("Size of a long long int: %d" , sizeof(long long int) , " bytes(s)\n");
+printf("Size of a float: %d" , sizeof(float) , " byte(s)\n");
 return (0);
 }
