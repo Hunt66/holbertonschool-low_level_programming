ab1c8135f4e29a805948581bf4d7de2a0186b625
Hunt6666 <489@holbertonschool.com>
2018-06-20 21:10:03 +0000

    file six first attempt using sizeof()

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -7,3 +7,10 @@
  */
 int main(void)
 {
+puts("Size of a char: " sizeof(char));
+puts("Size of an int: " sizeof(int));
+puts("Size of a long int: " sizeof(long int));
+puts("Size of a long long int: " sizeof(long long int));
+puts("Size of a float: " sizeof(float));
+return(0);
+}
