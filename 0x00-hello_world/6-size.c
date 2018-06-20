e84ef532132180188280c183e7a334dc99a7bd95
Hunt6666 <489@holbertonschool.com>
2018-06-20 21:52:23 +0000

    adding tabs finally

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -7,15 +7,15 @@
  */
 int main(void)
 {
-printf("Size of a char: %d", sizeof(char));
-printf(" byte(s)\n");
-printf("Size of an int: %d", sizeof(int));
-printf(" byte(s)\n");
-printf("Size of a long int: %d", sizeof(long int));
-printf(" byte(s)\n");
-printf("Size of a long long int: %d", sizeof(long long int));
-printf(" bytes(s)\n");
-printf("Size of a float: %d", sizeof(float));
-printf(" byte(s)\n");
-return (0);
+	printf("Size of a char: %d", sizeof(char));
+	printf(" byte(s)\n");
+	printf("Size of an int: %d", sizeof(int));
+	printf(" byte(s)\n");
+	printf("Size of a long int: %d", sizeof(long int));
+	printf(" byte(s)\n");
+	printf("Size of a long long int: %d", sizeof(long long int));
+	printf(" bytes(s)\n");
+	printf("Size of a float: %d", sizeof(float));
+	printf(" byte(s)\n");
+	return (0);
 }
