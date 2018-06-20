81740e0a1ccedba1dc0da641fabb8d39b605fa27
Hunt6666 <489@holbertonschool.com>
2018-06-20 19:07:55 +0000

    I am currently first attempting on files 0-6 I am trying to descover how to use the gcc command mostly I have little faith in this run.

---
+++ 0X00-hello_world/4-puts.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+/**
+ * main - prints "Programming is like building a multilingual puzzle
+ *
+ * Return: Always 0 (Success)
+ */
+
+int main(void)
+{
+  string puts("\"Programming is like building a multilingual puzzle") 
+
+
+
+
+ return (0);
+}
---
+++ 0X00-hello_world/6-size.c
@@ -0,0 +1,9 @@
+#include <stdio.h>
+
+/**
+ * main - printing the size of various data types on computers
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
