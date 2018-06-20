801215407ce25fc401cfbcb880a9edcbad09ba33
Hunt6666 <489@holbertonschool.com>
2018-06-20 21:14:35 +0000

    I need a space after return before (0)

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -12,5 +12,5 @@ puts("Size of an int: " sizeof(int));
 puts("Size of a long int: " sizeof(long int));
 puts("Size of a long long int: " sizeof(long long int));
 puts("Size of a float: " sizeof(float));
-return(0);
+return (0);
 }
