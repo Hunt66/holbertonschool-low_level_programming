9e0686e8464af83dd355f01279b78616fd7c0773
Hunt6666 <489@holbertonschool.com>
2018-06-21 21:04:17 +0000

    while(0) infinate loop not accepted in c only c++

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point  prints every lowercased letter of alphabet
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	char let;
+
+	let = 'a';
+	while (let < 'z'){
+		putchar(let);
+		let++;
+	}
+	putchar ('\n');
+
+	return (0);
+}
