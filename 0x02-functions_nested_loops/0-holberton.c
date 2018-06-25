e3f6c0a2bc9a8cab5c2e8ac91ac323cb2518f8c3
Hunt6666 <489@holbertonschool.com>
2018-06-25 19:29:38 +0000

    file 0 first attempt

---
+++ 0x02-functions_nested_loops/0-holberton.c
@@ -0,0 +1,16 @@
+# include <stdio.h>
+
+/*
+ * main- entry point accepts nothing
+ *       returns 0
+ *
+ * Return: alwayse 0
+ *
+ * does-prints Holberton and a new line
+ */
+
+int main(void)
+{
+	printf("Holberton\n");
+	return (0);
+}
