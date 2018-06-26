1799afab690d069392cf59d1e829ffba26509565
Hunt6666 <489@holbertonschool.com>
2018-06-26 19:26:14 +0000

    file 6 first attempt

---
+++ 0x02-functions_nested_loops/6-abs.c
@@ -0,0 +1,24 @@
+# include "holberton.h"
+
+/**
+ * _abs(int) - accepts int
+ *              returns absolute value
+ *@n: input int
+ * Return: alwayse absolute value
+ *
+ */
+
+
+
+int _abs(int n)
+{
+	int i;
+
+	if (n > 0)
+		i = n;
+	if (n == 0)
+		i = n;
+	if (n < 0)
+		i = (n * -1);
+	return (i);
+}
