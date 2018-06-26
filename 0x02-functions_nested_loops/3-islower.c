44a70b27849fafa10711008190f3f8cc0dfd89aa
Hunt6666 <489@holbertonschool.com>
2018-06-26 17:33:24 +0000

    changeing my holberton.h to only have prototypes not whole functions

--- 0x02-functions_nested_loops/3-islower.c
+++ 0x02-functions_nested_loops/3-islower.c
@@ -32,24 +32,3 @@ int _islower(int c)
 	}
 	return(i);
 }
-
-
-
-
-
-
-
-
-int main(void)
-{
-	int r;
-
-	r = _islower('H');
-	putchar(r + '0');
-	r = _islower('o');
-	putchar(r + '0');
-	r = _islower(108);
-	putchar(r + '0');
-	putchar('\n');
-	return (0);
-}
