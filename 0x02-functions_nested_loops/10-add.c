860b11e6974dbd5b7991ae1cf1117c7c80122e3b
Hunt6666 <489@holbertonschool.com>
2018-06-26 22:00:39 +0000

    file 10 first attempt it cant be this simple

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,18 @@
+# include "holberton.h"
+
+/**
+ * add - accepts two ints
+ *       returns the ints added
+ * @a: first input int
+ * @b: second input int to be added
+ *
+ * Return: alwayse added ints
+ *
+ */
+
+
+
+int add(int a, int b)
+{
+	return (a + b);
+}
