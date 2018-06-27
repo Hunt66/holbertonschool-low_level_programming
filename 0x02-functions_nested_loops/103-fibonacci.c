93c29c704420d8e1a28a5cebe4c19ee7a3855a8c
Hunt6666 <489@holbertonschool.com>
2018-06-27 21:23:55 +0000

    it is a randome number start all vars at a number like 0

--- 0x02-functions_nested_loops/103-fibonacci.c
+++ 0x02-functions_nested_loops/103-fibonacci.c
@@ -15,15 +15,16 @@ int main(void)
 	a = 0;
 	b = 1;
 	c = 0;
+	d = 0;
 	while (1)
 	{
 		c = a + b;
 		a = b;
 		b = c;
-		if (c % 2 == 0)
-			d += c;
 		if (c > 4000000)
 			break;
+		if ((c % 2) == 0)
+			d += c;
 	}
 	printf("%ld\n", d);
 	return (0);
