7d47c9323e9968f072e29c266335179fe6cb2e80
Hunt6666 <489@holbertonschool.com>
2018-06-28 20:38:08 +0000

    problem 9 first attempt fingers crossed

---
+++ 0x03-more_functions_nested_loops/9-fizz_buzz.c
@@ -0,0 +1,31 @@
+#include "holberton.h"
+# include <stdio.h>
+
+/**
+ *main - accepts void prints 1 to 100
+ *       but if something is divisable by 3
+ *       print fizz and buzz if 5 both
+ *       if devisable by both
+ *Return: 0
+ */
+
+int main(void)
+{
+	int i;
+
+	for (i = 0 ; i <= 100 ; i++)
+	{
+		if ((i % 3) == 0 && (i % 5) == 0)
+			printf("FizzBuzz ");
+		else if ((i % 3) == 0)
+			printf("Fizz ");
+		else if ((i % 5) == 0)
+			printf("Buzz ");
+		else
+		{
+			printf("%d ", i);
+		}
+	}
+	putchar('\n');
+	return (0);
+}
