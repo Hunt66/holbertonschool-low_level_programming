6031b8f3ee2f21be3b3358a595627ea6da9ae4c7
hunt6666 <489@holbertonschool.com>
2018-06-30 02:35:28 +0000

    I cannot get the pow function to work or the ** even though I included <math.h>. I am now saving so I can shut down vagrant and start it up again. I hope this will work. I wonder if I don't have the math.h to include, but that seems imposible. I guess I should test it though.

--- 0x03-more_functions_nested_loops/101-print_number.c
+++ 0x03-more_functions_nested_loops/101-print_number.c
@@ -1,5 +1,5 @@
-#include "holberton.h"
 #include <math.h>
+#include "holberton.h"
 
 /**
  * print_number(int n) - This funciton will print any input int n
@@ -22,12 +22,14 @@ void print_number(int n)
 		if (hold == 0)
 			break;
 		plc ++;
+		/*inc = (10 ** plc) / 1;*/
 		inc = pow(10, plc) / 1;
 	}
-	_putchar(n / (pow(10, plc) / 1) + '0');
-	for (y = plc ; y > 0 ; y--)
+	_putchar(n / ((pow(10, plc) / 1) + '0'));
+	for (y = plc - 1 ; y > 0 ; y--)
 	{
-		prt = n / (pow(10, (plc - 1)) / 1);
+		/*prt = n / (10 ** y) / 1);*/
+		prt = n / (pow(plc, y) / 1);
 		_putchar((prt % 10) + '0');
 	}
 }
