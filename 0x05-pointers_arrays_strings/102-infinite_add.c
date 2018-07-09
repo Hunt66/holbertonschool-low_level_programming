80f41afc97c9a2a6d92f4b723d1d1b5e34f06d61
Hunt6666 <489@holbertonschool.com>
2018-07-09 02:22:42 +0000

    Finally got this one to work I was having problems with carrying whith a bunch of nines when there was nothing else in the other string

--- 0x05-pointers_arrays_strings/102-infinite_add.c
+++ 0x05-pointers_arrays_strings/102-infinite_add.c
@@ -1,7 +1,7 @@
 #include"holberton.h"
 
 /**
- *infinite_add- This adds the numers stored on two strings adds them
+ *infinite_add- This adds the numers stored on two strings
  *@n1: one of the added numbers
  *@n2: the other added number
  *@r: pointer to string to store the added numbers
@@ -11,39 +11,124 @@
 
 char *infinite_add(char *n1, char *n2, char *r, int size_r)
 {
-	int i, j, x, s, h;
+	int i, j, l, t, c, cy, nt;
 
-	h = 0;
+	c = 0;
+	l = 1;
+	cy = 0;
 	for (i = 0 ; n1[i] != '\0' ; i++)
 		;
 	for (j = 0 ; n2[j] != '\0' ; j++)
 		;
-	if (size_r > i + 1 && size_r > j + 1)
-		return (0);
 	if (i >= j)
-		s = i;
-	else
-		s = j;
-	for (x = s ; x > 0 ; x--)
 	{
-		if (x > i)
-		{
-			r[x + 1] = n2[x] + h;
-			continue;
-		}
-		if (x > j)
-		{
-			r[x + 1] = n1[x] + h;
-			continue;
-		}
-		if (n1[x] + n2[x] + h > 9)
-		{
-			r[x + 1] = n1[x] + n2[x] + h - 10;
-			h = 1;
-			continue;
-		}
-		r[x] = n1[x] + n2[x] + h;
-		h = 0;
+		l = i;
 	}
-	return (r);
+	else if (j > i)
+	{
+		l = j;
+	}
+	i--;
+	j--;
+	if (size_r < l + 1)
+		return (0);
+	for (t = l - 1 ; t >= 0 ; t--)
+	{
+		if (i >= 0 && j >= 0)
+			cy = 1;
+		else if (i >= 0 && j < 0)
+			cy = 2;
+		else if (i < 0 && j >= 0)
+			cy = 3;
+		if (cy == 1)
+		{
+			if (((n1[i] - '0') + (n2[j] - '0') + c) > 9)
+			{
+				if (c == 1)
+				{
+					r[t] = (((((n1[i] - '0') +
+						   (n2[j] - '0')) - 9)) + '0');
+				}
+				else
+				{
+					r[t] = (((n1[i] - '0') +
+						   (n2[j] - '0') - 10) + '0');
+				}
+				c = 1;
+			}
+			else
+			{
+				if (c == 1)
+				{
+					r[t] = (((n1[i] - '0') +
+						(n2[j] - '0') + 1) + '0');
+				}
+				else
+				{
+					r[t] = ((n1[i] - '0') + (n2[j] - '0')
+					+ '0');
+				}
+				c = 0;
+			}
+		}
+		if (cy == 2)
+		{
+			if (c == 1)
+			{
+				if (n1[i] + 1 > '9')
+				{
+					r[t] = '0';
+					c = 1;
+				}
+				else
+				{
+					r[t] = (n1[i] + 1);
+					c = 0;
+				}
+			}
+			else
+			{
+				r[t] = n1[i];
+				c =  0;
+			}
+		}
+		if (cy == 3)
+		{
+			if (c == 1)
+			{
+				if (n2[j] + 1 > '9')
+				{
+					r[t] = '0';
+					c = 1;
+				}
+				else
+				{
+					r[t] = (n2[j] + 1);
+					c = 0;
+				}
+			}
+			else
+			{
+				r[t] = n2[j];
+				c = 0;
+			}
+		}
+		i--;
+		j--;
+	}
+	r[l] = '\0';
+	if (c == 1)
+	{
+		if (size_r < l + 2)
+			return (0);
+		for (nt = l ; nt >= 1 ; nt--)
+		{
+			r[nt] = r[nt - 1];
+		}
+		r[0] = '1';
+		r[l + 1] = '\0';
+	}
+	if (size_r >= l)
+		return (r);
+	return (0);
 }
