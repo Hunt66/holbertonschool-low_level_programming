a30b3ae01ec87b050c896df15d53e616ab200c62
Hunt6666 <489@holbertonschool.com>
2018-07-10 23:50:28 +0000

    Finally got the right output on this

--- 0x06-pointers_arrays_strings/8-print_diagsums.c
+++ 0x06-pointers_arrays_strings/8-print_diagsums.c
@@ -1,4 +1,45 @@
 #include"holberton.h"
+#include <stdio.h>
 
 /**
- *
+ *print_diagsums - prints the sum of the two diagonas of a
+ *                 square matrix of ints
+ *@a: accepted matrix pointer
+ *@size: size of diagonal
+ *Return: void
+ */
+
+
+void print_diagsums(int *a, int size)
+{
+	int i, j, s;
+
+	s = 0;
+	for (i = 0 ; i < size ; i++)
+	{
+		for (j = 0 ; j < size ; j++)
+		{
+			if (i == j)
+			{
+				a = a + (i * size) + j;
+				s = s + *a;
+				a = a - (i * size) - j;
+			}
+		}
+	}
+	printf("%d, ", s);
+	s = 0;
+	for (i = 0 ; i < size ; i++)
+	{
+		for (j = 0 ; j < size ; j++)
+		{
+			if (i == (size - 1) - j)
+			{
+				a = a + (i * size) + j;
+				s = s + *a;
+				a = a - (i * size) - j;
+			}
+		}
+	}
+	printf("%d\n", s);
+}
