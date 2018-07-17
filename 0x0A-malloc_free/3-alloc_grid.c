c05870ce6f0ec4fe627edc758b4b4e353e8b9fcc
Hunt6666 <489@holbertonschool.com>
2018-07-17 20:44:42 +0000

    cannot free out it is not possible

--- 0x0A-malloc_free/3-alloc_grid.c
+++ 0x0A-malloc_free/3-alloc_grid.c
@@ -1,5 +1,6 @@
 #include "holberton.h"
 #include <stdlib.h>
+#include <stdio.h>
 
 
 /**
@@ -13,15 +14,26 @@
 int **alloc_grid(int width, int height)
 {
 	int i, j;
-	int **out = (int **)malloc(width * height * sizeof(int *));
+	int **out = (int **)malloc(height * sizeof(int *));
 
 	if (out == NULL)
+	{
+		free(out);
 		return (NULL);
+	}
 	if (width <= 0 || height <= 0)
 		return (NULL);
 	for (i = 0 ; i < height ; i++)
 	{
 		out[i] = (int *)malloc(width * sizeof(int));
+		if (out[i] == NULL)
+		{
+			for (i = i - 1; i >= -1 ; i--)
+			{
+				free(out[i]);
+			}
+			return (NULL);
+		}
 		for (j = 0 ; j < width ; j++)
 		{
 			out[i][j] = 0;
