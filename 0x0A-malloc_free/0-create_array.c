77fcaca4c4767f1f1e688ae55fef346955e6f879
Hunt6666 <489@holbertonschool.com>
2018-07-17 17:37:40 +0000

    This is my first attempt of using memory allocation on problem 0

---
+++ 0x0A-malloc_free/0-create_array.c
@@ -0,0 +1,23 @@
+#include "holberton.h"
+#include <stdlib.h>
+
+/**
+ *create_array - creates an array of chars and initilizes it with a special char
+ *@size: the size of the array
+ *@c: the input charicter
+ *Return: NULL if size is 0 NULL if fails or a pointer to an array
+ */
+
+
+char *create_array(unsigned int size, char c)
+{
+	char *ar;
+	unsigned int i;
+
+	ar = malloc(size * sizeof(char));
+	for (i = 0 ; i <= size ; i++)
+	{
+		ar[i] = c;
+	}
+	return (ar);
+}
---
+++ 0x0A-malloc_free/README.md
@@ -0,0 +1,5 @@
+##Holberton Coding School Assignment 7/17/2018
+This directory has code that was made for the perpous of education
+#Malloc and Free
+These are my first programs learning how to use memory allocation
+I will be using malloc and free
