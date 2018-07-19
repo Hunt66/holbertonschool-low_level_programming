c254322b83a7c23bbb7b60c6865f20ce15555a94
Hunt6666 <489@holbertonschool.com>
2018-07-19 20:17:34 +0000

    Finally got bzero to work

--- 0x0B-more_malloc_free/2-calloc.c
+++ 0x0B-more_malloc_free/2-calloc.c
@@ -3,6 +3,26 @@
 
 
 /**
+ *_bzero - fills with zero
+ *@s1: string
+ *@n: number
+ *Return: void
+ */
+
+
+void _bzero(void *s1, unsigned int n)
+{
+	char *p;
+
+	for (p = s1; n--;)
+		*p++ = '\0';
+}
+
+
+
+
+
+/**
  *_calloc - alocates memory for an array using malloc
  *@nmemb: size of array
  *@size: size of data type
@@ -16,8 +36,9 @@ void *_calloc(unsigned int nmemb, unsigned int size)
 
 	if (size == 0 || nmemb == 0)
 		return (NULL);
-	out = malloc((nmemb + 1) * size);
+	out = malloc((nmemb) * size);
 	if (out == NULL)
 		return (NULL);
+	_bzero(out, size * nmemb);
 	return (out);
 }
