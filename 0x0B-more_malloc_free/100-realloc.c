75b98d962e2ca587b1c45e061d3941520524b568
Hunt6666 <489@holbertonschool.com>
2018-07-20 00:13:06 +0000

    I have no valgrind errors and I am getting the right output everything is being cleared

--- 0x0B-more_malloc_free/100-realloc.c
+++ 0x0B-more_malloc_free/100-realloc.c
@@ -30,13 +30,13 @@ void *_realloc(void *ptr, unsigned int old_size, unsigned int new_size)
 		free(ptr);
 		return (out);
 	}
-	if (old_size = new_size)
-		return (ptr);
 	else if (new_size == 0)
 	{
 		free(ptr);
 		return (NULL);
 	}
+	if (old_size == new_size)
+		return (ptr);
 	out = malloc(new_size);
 	if (out == NULL)
 	{
