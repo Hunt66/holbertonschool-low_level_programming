d9ea62697bfe6f59928130a7e0d993eb8f343cec
Hunt6666 <489@holbertonschool.com>
2018-07-23 18:20:08 +0000

    I forgot readme file and comments

---
+++ 0x0D-structures_typedef/README.md
@@ -0,0 +1,6 @@
+##Holberton Coding School
+#Structures and Typedef
+#Erwin Gove
+7/23/2018
+This directory is made for the perpous of education
+I will be making programs using structures and typedefs
--- 0x0D-structures_typedef/dog.h
+++ 0x0D-structures_typedef/dog.h
@@ -1,6 +1,16 @@
 #ifndef DOG_H
 #define DOG_H
 
+/**
+ *dog - creates a dog type kinda like an object in c++
+ *@name: name of dog
+ *@age: age of dog
+ *@owner: owner of dog
+ *
+ *Description: makes basicly an object dog with various data types
+ */
+
+
 struct dog
 {
 	char *name;
