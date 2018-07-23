d288c938b772398e749d6805c3c38e35ba8ef872
Hunt6666 <489@holbertonschool.com>
2018-07-23 19:13:10 +0000

    I need to put the derefrenced pointer in parenthacies I guess

---
+++ 0x0D-structures_typedef/1-init_dog.c
@@ -0,0 +1,17 @@
+#include "dog.h"
+
+/**
+ *init_dog - initializes a variable of type dog which I just made
+ *@d: pointer to structure dog
+ *@name:pointer to name of dog
+ *@age: age of dog
+ *@owner: pointer to owner name of dog
+ *Return: void
+ */
+
+void init_dog(struct dog *d, char *name, float age, char *owner)
+{
+	(*d).name = name;
+	(*d).age = age;
+	(*d).owner = owner;
+}
