c80dc27787e138da16b1de67618588a05ee65b01
Hunt6666 <489@holbertonschool.com>
2018-07-23 20:58:27 +0000

    I only have three frees but here we go anyway

--- 0x0D-structures_typedef/5-free_dog.c
+++ 0x0D-structures_typedef/5-free_dog.c
@@ -2,3 +2,15 @@
 #include<stdio.h>
 #include"dog.h"
 
+/**
+ *free_dog - frees dogs
+ *@d: pointer to dog_t we need to free
+ *Return: void
+ */
+
+void free_dog(dog_t *d)
+{
+	free((*d).name);
+	free((*d).owner);
+	free(d);
+}
