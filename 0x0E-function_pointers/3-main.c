2ccb6bdf157962bf515c919e778d5f3bc9101c4b
Hunt6666 <489@holbertonschool.com>
2018-07-25 21:08:14 +0000

    did not make sure that there was only one thing in argv[2]

--- 0x0E-function_pointers/3-main.c
+++ 0x0E-function_pointers/3-main.c
@@ -22,6 +22,11 @@ int main(int argc, char **argv)
 		printf("Error\n");
 		exit(98);
 	}
+	if (argv[2][1] != '\0')
+	{
+		printf("Error\n");
+		exit(98);
+	}
 	a = atoi(argv[1]);
 	b = atoi(argv[3]);
 	ans = ((get_op_func(argv[2]))(a, b));
