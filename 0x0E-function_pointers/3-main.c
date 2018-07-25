3c668a65e50e2cbd62788e45e51157f9c54f18bf
Hunt6666 <489@holbertonschool.com>
2018-07-25 21:14:13 +0000

    wrong exit number again

--- 0x0E-function_pointers/3-main.c
+++ 0x0E-function_pointers/3-main.c
@@ -21,7 +21,7 @@ int main(int argc, char **argv)
 		printf("Error\n");
 		exit(98);
 	}
-	if(argv[2][0] != '*' && argv[2][0] != '-' && argv[2][0]
+	if (argv[2][0] != '*' && argv[2][0] != '-' && argv[2][0]
 			  != '+' && argv[2][0] != '%' && argv[2][0] != '/')
 	{
 		printf("Error\n");
@@ -30,7 +30,7 @@ int main(int argc, char **argv)
 	if (argv[2][1] != '\0')
 	{
 		printf("Error\n");
-		exit(98);
+		exit(99);
 	}
 	a = atoi(argv[1]);
 	b = atoi(argv[3]);
