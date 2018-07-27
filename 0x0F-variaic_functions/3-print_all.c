64be15fda6d20ba8e3cce5c8ae9e6ddc39565e62
Hunt6666 <489@holbertonschool.com>
2018-07-27 21:25:02 +0000

    I finally got it to work but floats have trailing 0s so that might screw up checker

--- 0x0F-variaic_functions/3-print_all.c
+++ 0x0F-variaic_functions/3-print_all.c
@@ -21,72 +21,51 @@ void print_all(const char * const format, ...)
 	va_start(list, format);
 
 
-	while (format[i + 2] != '\0')
+	while (format[i + 1] != '\0')
 	{
-		switch(format[i])
+		switch (format[i])
 		{
 		case 'c':
-		{
-			printf("c\n");
 			printf("%c, ", (char)va_arg(list, int));
-		}
+			break;
 		case 'i':
-		{
-			printf("i\n");
 			printf("%d, ", va_arg(list, int));
-		}
+			break;
 		case 'f':
-		{
-			printf("f\n");
 			printf("%f, ", va_arg(list, double));
-		}
+			break;
 		case 's':
-		{
-			printf("s\n");
 			str = va_arg(list, char *);
 			if (str == NULL)
 			{
-				printf("sn\n");
 				printf("(nil), ");
 				i++;
-				continue;
+				break;
 			}
-			printf("snn\n");
-			printf("%s, ", (char *)va_arg(list, char *));
-		}
+			printf("%s, ", str);
+			break;
 		}
 		i++;
 	}
 	switch(format[i])
 	{
-		printf("format[i]=%c\n", format[i]);
 	case 'c':
-	{
-		printf("c2\n");
 		printf("%c\n", (char)va_arg(list, int));
-	}
+		break;
 	case 'i':
-	{
-		printf("i2\n");
 		printf("%i\n", va_arg(list, int));
-	}
+		break;
 	case 'f':
-	{
-		printf("i2\n");
 		printf("%f\n", va_arg(list, double));
-	}
+		break;
 	case 's':
-	{
-		printf("s2\n");
 		str = va_arg(list, char *);
 		if (str == NULL)
 		{
-			printf("sn2\n");
 			printf("(nil)\n");
 			break;
 		}
-		printf("snn2\n");
-		printf("%s\n", (char *)va_arg(list, char *));
-	}
+		printf("%s\n", str);
+		break;
 	}
 }
