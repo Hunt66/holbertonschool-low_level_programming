e370c0581d13ad0a6b7141bbb0d8c9d3f7e94538
Hunt6666 <489@holbertonschool.com>
2018-07-27 20:45:28 +0000

    I am getting the wierdest errors my switch statements are not working right

--- 0x0F-variaic_functions/3-print_all.c
+++ 0x0F-variaic_functions/3-print_all.c
@@ -15,38 +15,78 @@ void print_all(const char * const format, ...)
 {
 	va_list list;
 	unsigned int i = 0;
-	char c;
 	char *str = "";
 
-	va_start(list, n);
 
-	while (format[i + 1] != '\0')
+	va_start(list, format);
+
+
+	while (format[i + 2] != '\0')
 	{
 		switch(format[i])
 		{
 		case 'c':
 		{
-			printf("%c, ", va_arg(list, char));
+			printf("c\n");
+			printf("%c, ", (char)va_arg(list, int));
 		}
 		case 'i':
 		{
-			printf("%i, ", va_arg(list, int));
+			printf("i\n");
+			printf("%d, ", va_arg(list, int));
 		}
 		case 'f':
 		{
-			printf("%f, ", va_arg(list, float));
+			printf("f\n");
+			printf("%f, ", va_arg(list, double));
 		}
 		case 's':
 		{
+			printf("s\n");
 			str = va_arg(list, char *);
 			if (str == NULL)
 			{
+				printf("sn\n");
 				printf("(nil), ");
 				i++;
 				continue;
 			}
-			printf("%s, ", va_arg(list, char *));
+			printf("snn\n");
+			printf("%s, ", (char *)va_arg(list, char *));
 		}
 		}
 		i++;
-		
+	}
+	switch(format[i])
+	{
+		printf("format[i]=%c\n", format[i]);
+	case 'c':
+	{
+		printf("c2\n");
+		printf("%c\n", (char)va_arg(list, int));
+	}
+	case 'i':
+	{
+		printf("i2\n");
+		printf("%i\n", va_arg(list, int));
+	}
+	case 'f':
+	{
+		printf("i2\n");
+		printf("%f\n", va_arg(list, double));
+	}
+	case 's':
+	{
+		printf("s2\n");
+		str = va_arg(list, char *);
+		if (str == NULL)
+		{
+			printf("sn2\n");
+			printf("(nil)\n");
+			break;
+		}
+		printf("snn2\n");
+		printf("%s\n", (char *)va_arg(list, char *));
+	}
+	}
+}
--- 0x0F-variaic_functions/main.c
+++ 0x0F-variaic_functions/main.c
@@ -1,5 +1,4 @@
-#include"variadic_functions.h"
-
+#include "variadic_functions.h"
 
 /**
  * main - check the code for Holberton School students.
@@ -8,6 +7,6 @@
  */
 int main(void)
 {
-	print_strings(", ", 2, "Jay", "Django");
+	print_all("ceis", 'H', 0, "lberton");
 	return (0);
 }
