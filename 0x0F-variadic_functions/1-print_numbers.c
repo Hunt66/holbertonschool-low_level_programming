15f94512b0b7e549b5140f8f5bfb231622fe494d
Hunt6666 <489@holbertonschool.com>
2018-07-29 16:49:36 +0000

    Calling va_args only once this time

--- 0x0F-variadic_functions/1-print_numbers.c
+++ 0x0F-variadic_functions/1-print_numbers.c
@@ -22,7 +22,10 @@ void print_numbers(const char *separator, const unsigned int n, ...)
 	{
 		for (i = 0 ; i < n - 1 ; i++)
 		{
-			printf("%d%s", va_arg(list, int), separator);
+			printf("%d", va_arg(list, int));
+			if (i == n - 2)
+				break;
+			printf("%s", separator);
 		}
 	}
 	else
@@ -32,7 +35,7 @@ void print_numbers(const char *separator, const unsigned int n, ...)
 			printf("%d", va_arg(list, int));
 		}
 	}
-	printf("%d\n", va_arg(list, int));
+	printf("\n");
 
 	va_end(list);
 }
