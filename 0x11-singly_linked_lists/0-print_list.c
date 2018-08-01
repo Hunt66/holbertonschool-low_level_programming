b34d8fa481dec3e7e26e2ebce336c9f04b3f0d01
Hunt6666 <489@holbertonschool.com>
2018-08-01 02:03:11 -0700

    got number 0 file to print expected

---
+++ 0x11-singly_linked_lists/0-print_list.c
@@ -0,0 +1,25 @@
+#include"lists.h"
+#include<stdio.h>
+#include <stddef.h>
+#include <stdlib.h>
+
+/**
+ *print_list- prints all elements of a list_t list
+ *@h: the list to be printed
+ *Return: number of nodes in list
+ */
+
+
+size_t print_list(const list_t *h)
+{
+	if (h == NULL)
+		return (1);
+	if (h->str == NULL)
+		printf("[0] (nil)\n");
+	else
+	{
+		printf("[%d] ",h->len);
+		printf("%s\n", h->str);
+	}
+	return (print_list(h->next) + 1);
+}
---
+++ 0x11-singly_linked_lists/README.md
@@ -0,0 +1,10 @@
+##Holberton Coding School
+#Sigly Linked Lists
+8/2/2018
+By Erwin Gove
+
+This directory is made for the perpoius of education
+
+In this lession I will be making and using linked lists
+I will be making various programs that will add things, print
+and various other tasks to singly linked lists should be fun
