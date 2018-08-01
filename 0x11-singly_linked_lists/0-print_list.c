376e31aee1dfd5839e9b531e9b37fa1c3f9cb39a
Hunt6666 <489@holbertonschool.com>
2018-08-01 03:14:56 -0700

    betty is wrong

--- 0x11-singly_linked_lists/0-print_list.c
+++ 0x11-singly_linked_lists/0-print_list.c
@@ -12,13 +12,14 @@
 
 size_t print_list(const list_t *h)
 {
+
 	if (h == NULL)
 		return (0);
 	if (h->str == NULL)
 		printf("[0] (nil)\n");
 	else
 	{
-		printf("[%d] ",h->len);
+		printf("[%d] ", h->len);
 		printf("%s\n", h->str);
 	}
 	return (print_list(h->next) + 1);
