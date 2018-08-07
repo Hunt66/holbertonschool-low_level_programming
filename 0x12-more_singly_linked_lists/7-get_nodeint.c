2d5b32b4f6dd7fb70fe5260037a79d6259d96d2c
Hunt6666 <489@holbertonschool.com>
2018-08-07 10:35:41 -0700

    I am getting a seg fault that may happen when I return NULL

--- 0x12-more_singly_linked_lists/7-get_nodeint.c
+++ 0x12-more_singly_linked_lists/7-get_nodeint.c
@@ -13,15 +13,19 @@
 
 listint_t *get_nodeint_at_index(listint_t *head, unsigned int index)
 {
-	unsigned int i;
+	unsigned int i = 0;
 	listint_t *current;
 
-	if (!head)
+	if (head == NULL)
 		return (NULL);
 
 	current = head;
 	for (i = 0 ; i <= index ; i++)
 	{
+		if (current->next == NULL)
+		{
+			return (NULL);
+		}
 		current = current->next;
 	}
 
