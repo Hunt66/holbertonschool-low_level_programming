a67a04d79598d84953f4a224d301496a8702f6cf
Hunt6666 <489@holbertonschool.com>
2018-08-07 10:38:05 -0700

    returning wrong node by one

--- 0x12-more_singly_linked_lists/7-get_nodeint.c
+++ 0x12-more_singly_linked_lists/7-get_nodeint.c
@@ -20,7 +20,7 @@ listint_t *get_nodeint_at_index(listint_t *head, unsigned int index)
 		return (NULL);
 
 	current = head;
-	for (i = 0 ; i <= index ; i++)
+	for (i = 0 ; i < index ; i++)
 	{
 		if (current->next == NULL)
 		{
