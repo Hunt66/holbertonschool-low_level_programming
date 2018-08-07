8e1fade1c705bc2faf87c2e8f523cdb1c1b82967
Hunt6666 <489@holbertonschool.com>
2018-08-07 10:44:38 -0700

    adding check for failed mallco

--- 0x12-more_singly_linked_lists/9-insert_nodeint.c
+++ 0x12-more_singly_linked_lists/9-insert_nodeint.c
@@ -16,21 +16,21 @@ listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n)
 {
 	unsigned int i;
 	listint_t *new = malloc(sizeof(listint_t));
-	listint_t *current = *head;
-	/*listint_t *next = *head;*/
+	listint_t *current;
 
+	if (new == NULL)
+		return (NULL);
 
 	if (head == NULL)
 		return (NULL);
 
-	/*new = *head;*/
+	current = *head;
+
 	for (i = 0 ; i < idx - 1 ; i++)
 	{
 		if (current->next == NULL)
 			return (NULL);
 		current = current->next;
-		/*new = new->next;*/
-		/*next = next->next;*/
 	}
 	if (current->next == NULL)
 		return (NULL);
