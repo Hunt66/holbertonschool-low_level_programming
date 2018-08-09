0225b31a7aa384a34c983f95cc271ddee007f8c8
Hunt6666 <489@holbertonschool.com>
2018-08-08 19:35:29 -0700

    I was not using bit manipulation now I am

--- 0x13-bit_manipulation/1-print_binary.c
+++ 0x13-bit_manipulation/1-print_binary.c
@@ -9,20 +9,23 @@
 
 void print_binary(unsigned long int n)
 {
-	unsigned long int div;
+	unsigned long int i;
+	unsigned long int  msk = 1024;
+	int flag = 0;
 
-	if (n == 0)
+	for (i = 0 ; i < 10; i++)
 	{
+		if (n & msk)
+		{
+			_putchar('1');
+			flag = 1;
+		}
+		else if (flag == 1)
+			_putchar('0');
+		msk = msk >> 1;
+	}
+	if (n & msk)
+		_putchar('1');
+	else
 		_putchar('0');
-		return;
-	}
-
-	for (div = 1 ; div <= n ; div = div * 2)
-		;
-	for (div = div / 2 ; div > 1 ; div = div / 2)
-	{
-		_putchar((n / div) + '0');
-		n = (n % div);
-	}
-	_putchar((n / div) + '0');
 }
