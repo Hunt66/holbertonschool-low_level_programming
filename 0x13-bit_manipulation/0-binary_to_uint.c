682784d3c774c05e86e182ca1f62984e904f3711
Hunt6666 <489@holbertonschool.com>
2018-08-08 18:52:46 -0700

    making test for empty string case

--- 0x13-bit_manipulation/0-binary_to_uint.c
+++ 0x13-bit_manipulation/0-binary_to_uint.c
@@ -17,12 +17,16 @@ unsigned int binary_to_uint(const char *b)
 
 	if (b == NULL)
 		return (ans);
+/*checks to see if b has anything*/
+	if (b[0] == '\0')
+		return (0);
 
+/*itterating to end of string*/
 	for (i = 0 ; b[i] != '\0' ; i++)
 		if (b[i] != '0' && b[i] != '1')
 			return (0);
 	i--;
-
+/*conversion*/
 	for (mul = 1 ; i >= 1 ; i--, mul = mul * 2)
 		ans += (b[i] - '0') * mul;
 	ans += (b[i] - '0') * mul;
