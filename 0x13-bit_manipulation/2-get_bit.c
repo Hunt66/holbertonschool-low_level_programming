78210adb501f9505361ef8ff724fccac33cd3f8b
Hunt6666 <489@holbertonschool.com>
2018-08-09 15:20:30 -0700

    directions are incorect

--- 0x13-bit_manipulation/2-get_bit.c
+++ 0x13-bit_manipulation/2-get_bit.c
@@ -12,13 +12,11 @@ int get_bit(unsigned long int n, unsigned int index)
 {
 	unsigned long int i;
 
-	if (n == 0 && index != 0)
-		return (-1);
 
 	for (i = 0 ; i < index ; i++)
 	{
 		if (n >> 1 == 0)
-			return (-1);
+			return (0);
 		n = n >> 1;
 	}
 	return (n & 1);
