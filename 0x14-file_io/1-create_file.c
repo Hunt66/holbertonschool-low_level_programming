24f5ecff97de6196c3407cd90748c739c523446d
Hunt6666 <489@holbertonschool.com>
2018-08-14 15:49:56 -0700

    aifhd

--- 0x14-file_io/1-create_file.c
+++ 0x14-file_io/1-create_file.c
@@ -19,7 +19,7 @@ int create_file(const char *filename, char *text_content)
 
 	if (!buf1)
 		return (-1);
-	fd = open(filename, O_CREAT | O_RDWR | O_TRUNC, 0600
+	fd = open(filename, O_CREAT | O_RDWR | O_TRUNC, 0600)
 	if (filename == NULL)
 	{
 
