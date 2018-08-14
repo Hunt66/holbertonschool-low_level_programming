9876e90f3273ce5c547b887f37b377ed3f5e9ad1
Hunt6666 <489@holbertonschool.com>
2018-08-14 13:11:16 -0700

    all right checking for size diffrences on read and write this should be it

--- 0x14-file_io/0-read_textfile.c
+++ 0x14-file_io/0-read_textfile.c
@@ -14,7 +14,9 @@ ssize_t read_textfile(const char *filename, size_t letters)
 {
 	int fd = open(filename, O_RDWR);
 	char *buf1;
-	ssize_t x;
+	/*ssize_t x;*/
+	int read_s, write_s;
+
 	if (filename == NULL)
 		return (0);
 	if (fd == -1)
@@ -23,7 +25,15 @@ ssize_t read_textfile(const char *filename, size_t letters)
 	buf1 = malloc(letters);
 	if (!buf1)
 		return (0);
-	x = write(1, buf1, read(fd, buf1, letters));
-	close (fd);
-	return (x);
+	read_s = read(fd, buf1, letters);
+	write_s = write(STDOUT_FILENO, buf1, read_s);
+	/*x = write(1, buf1, read(fd, buf1, letters));*/
+	if (write_s < read_s)
+	{
+		free(buf1);
+		return (0);
+	}
+	free(buf1);
+	close(fd);
+	return (write_s);
 }
