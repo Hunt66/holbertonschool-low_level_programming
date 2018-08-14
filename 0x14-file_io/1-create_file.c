393e5ccbc59f33c389b23d7fabc4f063078b908b
Hunt6666 <489@holbertonschool.com>
2018-08-14 15:55:31 -0700

    afsdhl;

--- 0x14-file_io/1-create_file.c
+++ 0x14-file_io/1-create_file.c
@@ -13,21 +13,20 @@ int create_file(const char *filename, char *text_content)
   int fd;/* = open(filename, O_CREAT | O_RDWR| O_TRUNC, 0600);*/
 	char *buf1;
 
+	if (!filename)
+	  return(NULL);
+	buf1 = malloc(sizeof(text_content));
+	fd = open(filename, O_CREATE | O_RDWR | OTRUNC, 0600);
 	if (fd == -1)
 		return (-1);
-	buf1 = malloc(sizeof(text_content));
+
 
 	if (!buf1)
 		return (-1);
-	fd = open(filename, O_CREAT | O_RDWR | O_TRUNC, 0600)
-	if (filename == NULL)
-	{
 
-		return (-1);
-	}
 
 	write(fd, buf1, sizeof(text_content));
-
+	free(buf);
 	close(fd);
 	return (1);
 }
