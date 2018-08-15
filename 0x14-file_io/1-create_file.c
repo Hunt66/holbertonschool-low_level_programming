4cddde64953b70c127e2084d6300d175dcee4845
Hunt6666 <489@holbertonschool.com>
2018-08-14 18:44:15 -0700

    Adding case for working NULL text case as well as adding the correct number of text

--- 0x14-file_io/1-create_file.c
+++ 0x14-file_io/1-create_file.c
@@ -10,28 +10,41 @@
 
 int create_file(const char *filename, char *text_content)
 {
-	int fd;/* = open(filename, O_CREAT | O_RDWR| O_TRUNC, 0600);*/
+	int fd, test, i;
 	char *buf1;
 
-	if (!text_content)
-		return (-1);
 	if (!filename)
 		return (-1);
+	if (!text_content)
+	{
+		fd = open(filename, O_CREAT | O_RDWR | O_TRUNC, 0600);
+		close(fd);
+		return (1);
+	}
+
 	buf1 = malloc(sizeof(text_content));
 	if (!buf1)
 	{
 		return (-1);
 	}
 
-	fd = open(filename, O_CREAT | O_RDWR | O_TRUNC, 0600);
+	fd = open(filename, O_TRUNC | O_CREAT | O_RDWR | O_TRUNC, 0600);
 	if (fd == -1)
 	{
 		free(buf1);
 		return (-1);
 	}
 
+	for (i = 0 ; text_content[i] ; i++)
+		;
 
-	write(fd, buf1, sizeof(text_content));
+	test = write(fd, buf1, i);
+	if (test == -1)
+	{
+		free(buf1);
+		return (-1);
+	}
+
 	free(buf1);
 	close(fd);
 	return (1);
