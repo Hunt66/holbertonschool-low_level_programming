2737800a820f9cf9986f8e046922bf1fb304c3bf
Hunt6666 <489@holbertonschool.com>
2018-08-14 17:56:53 -0700

    finally got my computer working again and will no longer be posting trash test commits

--- 0x14-file_io/1-create_file.c
+++ 0x14-file_io/1-create_file.c
@@ -13,16 +13,22 @@ int create_file(const char *filename, char *text_content)
 	int fd;/* = open(filename, O_CREAT | O_RDWR| O_TRUNC, 0600);*/
 	char *buf1;
 
+	if (!text_content)
+		return (-1);
 	if (!filename)
-		return(-1);
+		return (-1);
 	buf1 = malloc(sizeof(text_content));
+	if (!buf1)
+	{
+		return (-1);
+	}
+
 	fd = open(filename, O_CREAT | O_RDWR | O_TRUNC, 0600);
 	if (fd == -1)
+	{
+		free(buf1);
 		return (-1);
-
-
-	if (!buf1)
-		return (-1);
+	}
 
 
 	write(fd, buf1, sizeof(text_content));
