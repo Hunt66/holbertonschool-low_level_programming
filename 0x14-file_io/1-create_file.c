776036af69cedac247f9b184af232cafb6e5dea7
Hunt6666 <489@holbertonschool.com>
2018-08-14 19:46:01 -0700

    Got rid of malloc and fixed writing

--- 0x14-file_io/1-create_file.c
+++ 0x14-file_io/1-create_file.c
@@ -3,7 +3,7 @@
 /**
  *create_file- creates a file
  *@filename: name of file to be made
- *@text_content- the text to add to file
+ *@text_content: the text to add to file
  *Return: 1 on success -1 on fail
  */
 
@@ -11,7 +11,7 @@
 int create_file(const char *filename, char *text_content)
 {
 	int fd, test, i;
-	char *buf1;
+
 
 	if (!filename)
 		return (-1);
@@ -22,30 +22,23 @@ int create_file(const char *filename, char *text_content)
 		return (1);
 	}
 
-	buf1 = malloc(sizeof(text_content));
-	if (!buf1)
-	{
-		return (-1);
-	}
 
-	fd = open(filename, O_TRUNC | O_CREAT | O_RDWR | O_TRUNC, 0600);
+
+	fd = open(filename, O_CREAT | O_RDWR | O_TRUNC, 0600);
 	if (fd == -1)
 	{
-		free(buf1);
 		return (-1);
 	}
 
 	for (i = 0 ; text_content[i] ; i++)
 		;
 
-	test = write(fd, buf1, i);
+	test = write(fd, text_content, i);
 	if (test == -1)
 	{
-		free(buf1);
 		return (-1);
 	}
 
-	free(buf1);
 	close(fd);
 	return (1);
 }
