d75b716510ad78935b1d6f2010ed3ecbf51d770f
Hunt6666 <489@holbertonschool.com>
2018-11-16 13:55:40 -0800

    out

--- 0x1B-makefiles/5-island_perimeter.py
+++ 0x1B-makefiles/5-island_perimeter.py
@@ -7,12 +7,12 @@ def island_perimeter(grid):
     for i in range(0, len(grid) - 1):
         for j in range(0, len(grid[i])):
             if grid[i][j] == 1:
-                if j - 1 >= 0 and grid[i][j - 1] == 0:
+                if grid[i][j - 1] == 0:
                     count += 1
-                if i - 1 >= 0 and grid[i - 1][j] == 0:
+                if grid[i - 1][j] == 0:
                     count += 1
-                if i + 1 <= (len(grid) - 1) and grid[i + 1][j] == 0:
+                if grid[i + 1][j] == 0:
                     count += 1
-                if i + 1 <= (len(grid) - 1) and grid[i][j + 1] == 0:
+                if grid[i][j + 1] == 0:
                     count += 1
     return count
