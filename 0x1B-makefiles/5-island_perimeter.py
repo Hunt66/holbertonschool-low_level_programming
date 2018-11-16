0842a30fc0650796ffe755d47518db289ad3f05f
Hunt6666 <489@holbertonschool.com>
2018-11-16 14:47:17 -0800

    this should work and in fact does

--- 0x1B-makefiles/5-island_perimeter.py
+++ 0x1B-makefiles/5-island_perimeter.py
@@ -4,8 +4,8 @@
 def island_perimeter(grid):
     """finds the perimeter of an island of 1's"""
     count = 0
-    for i in range(0, len(grid)):
-        for j in range(0, len(grid[i])):
+    for i in range(len(grid)):
+        for j in range(len(grid[i])):
             if grid[i][j] == 1:
                 if j - 1 >= 0 and grid[i][j - 1] == 0:
                     count += 1
@@ -13,6 +13,6 @@ def island_perimeter(grid):
                     count += 1
                 if i + 1 <= (len(grid)) and grid[i + 1][j] == 0:
                     count += 1
-                if j + 1 <= (len(grid[i])) and grid[i][j + 1] == 0:
+                if j + 1 <= (len(grid[0])) and grid[i][j + 1] == 0:
                     count += 1
     return count
