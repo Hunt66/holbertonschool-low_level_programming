0bf6853319612be2b7e125fed9e8a25611743aa9
Hunt6666 <489@holbertonschool.com>
2018-11-16 13:58:28 -0800

    nice

--- 0x1B-makefiles/5-island_perimeter.py
+++ 0x1B-makefiles/5-island_perimeter.py
@@ -1,18 +1,17 @@
 #!/usr/bin/python3
 
-
 def island_perimeter(grid):
     """finds the perimeter of an island of 1's"""
     count = 0
-    for i in range(0, len(grid) - 1):
-        for j in range(0, len(grid[i])):
+    for i in range(0, len(grid) -1):
+        for j in range(0, len(gird[i])):
             if grid[i][j] == 1:
-                if grid[i][j - 1] == 0:
+                if j - 1 >= 0 and grid[i][j - 1] == 0:
                     count += 1
-                if grid[i - 1][j] == 0:
+                if i - 1 >= 0 and grid[i - 1][j] == 0:
                     count += 1
-                if grid[i + 1][j] == 0:
+                if i + 1 <= (len(grid) - 1) and grid[i + 1][j] == 0:
                     count += 1
-                if grid[i][j + 1] == 0:
+                if i + 1 <= (len(grid[i]) - 1) and grid[i][j + 1] == 0:
                     count += 1
     return count
