6ed866d33e0c03992d076299f4c066ec7606dfc8
Hunt6666 <489@holbertonschool.com>
2018-12-12 12:11:44 -0800

    betty

--- 0x1C-binary_trees/18-binary_tree_uncle.c
+++ 0x1C-binary_trees/18-binary_tree_uncle.c
@@ -11,7 +11,7 @@
 
 binary_tree_t *binary_tree_uncle(binary_tree_t *node)
 {
-       	binary_tree_t *temp;
+	binary_tree_t *temp;
 
 	if (node == NULL || !node->parent || node->parent->parent == NULL)
 		return (NULL);
