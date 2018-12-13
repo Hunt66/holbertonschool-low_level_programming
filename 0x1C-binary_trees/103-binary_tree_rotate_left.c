35a3ab18361ef9abefd94cb79939af92a498aa91
Hunt6666 <489@holbertonschool.com>
2018-12-13 14:54:42 -0800

    rotate

--- 0x1C-binary_trees/103-binary_tree_rotate_left.c
+++ 0x1C-binary_trees/103-binary_tree_rotate_left.c
@@ -3,22 +3,21 @@
 #include "binary_trees.h"
 #include <stdio.h>
 
-/**
- *binary_tree_is_complete- checks if the binary tree is compleat
- *
- */
-
 binary_tree_t *binary_tree_rotate_left(binary_tree_t *tree)
 {
-	binary_tree_t *temp, *temp2;
+	binary_tree_t *temp;
+
+	if (!tree)
+		return (NULL);
+	if (!tree->right)
+		return (NULL);
 
 	temp = tree->right;
+	temp->parent = tree->parent;
 	tree->right = temp->left;
-	temp2 = tree->parent;
-	temp->parent= temp2;
-	tree->right->parent = tree;
-	tree->parent = temp;
-	temp->right = tree;
-	tree = temp;
-	return (tree);
+	if (temp->left != NULL)
+		temp->left->parent = tree;
+	temp->left = tree;
+	temp->left->parent = temp;
+	return (temp);
 }
---
+++ 0x1C-binary_trees/104-binary_tree_rotate_right.c
@@ -0,0 +1,29 @@
+#include <stddef.h>
+#include <stdlib.h>
+#include "binary_trees.h"
+#include <stdio.h>
+
+
+/**
+ * binary_tree_rotate_right- performs a right rotation on a binary tree
+ * @tree: pointer to the root node of the tree to rotate
+ * Return: pointer to the new root node of the tree once rotated
+ */
+binary_tree_t *binary_tree_rotate_right(binary_tree_t *tree)
+{
+	binary_tree_t *temp;
+
+	if (tree == NULL)
+		return (NULL);
+	if (tree->left == NULL)
+		return (tree);
+
+	temp = tree->left;
+	temp->parent = tree->parent;
+	tree->left = temp->right;
+	if (temp->right != NULL)
+		temp->right->parent = tree;
+	temp->right = tree;
+	temp->right->parent = temp;
+	return (temp);
+}
